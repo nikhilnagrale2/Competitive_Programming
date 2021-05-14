b3c438da440740eb62adcc21983664856993a4ca
Nikhil Nagrale <nknagrale634@gmail.com>
2021-05-14 14:51:15 +0530

    Packing Rectangels

--- Edu/Binary_Search/2.1_Packing_Rectangles.cpp
+++ Edu/Binary_Search/2.1_Packing_Rectangles.cpp
@@ -1,5 +1,6 @@
 /*
-  Problem Link    :   https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/A
+  Problem Link    :
+  https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/A
   Contest Link    :   https://codeforces.com/edu/course/2/lesson/6/2/practice
   Status          :   Accepted
 
@@ -8,7 +9,7 @@
   Codechef        :   https://www.codechef.com/users/nikhilnagrale2
   Github          :   https://github.com/nikhilnagrale2
 
-  Copyright       :   Study and Solve. :) 
+  Copyright       :   Study and Solve. :)
 */
 
 #include <bits/stdc++.h>
@@ -16,30 +17,21 @@ using namespace std;
 
 long long w, h, n;
 
-bool good(long long m)
-{
-	return (m / w) * (m / h) >= n;
-}
+bool good(long long m) { return (m / w) * (m / h) >= n; }
 
-int main()
-{
-	cin >> w >> h >> n;
-	long long l = 0, r = 1;
-	while (!good(r))
-		r *= 2;
+int main() {
+    cin >> w >> h >> n;
+    long long l = 0, r = 1;
+    while (!good(r)) r *= 2;
 
-	while (r > l + 1)
-	{
-		long long m = (l + r) / 2;
-		if (good(m))
-		{
-			r = m;
-		}
-		else
-		{
-			l = m;
-		}
-	}
-	cout << r << "\n";
-	return 0;
+    while (r > l + 1) {
+        long long m = (l + r) / 2;
+        if (good(m)) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    cout << r << "\n";
+    return 0;
 }
